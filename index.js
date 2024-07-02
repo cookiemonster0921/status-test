ebf044b3d957578105d03bab292cdcf33d6faa6e
cookiemonster0921 <142496173+cookiemonster0921@users.noreply.github.com>
2024-07-02 15:45:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-const express = require('express');
-const { Client } = require('discord.js');
+const express = require("express");
+const { Client } = require("discord.js");
 
 const app = express();
 const port = 3000;
-const request = require('request')
-const client = new Client({ intents: ["Guilds", "GuildMessages", "GuildMembers", "GuildPresences"] });
+const request = require("request");
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "GuildMembers", "GuildPresences"],
+});
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(process.env.token);
-var webhook = process.env.webhook
+var webhook = process.env.webhook;
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.status(403).send('forbidden')
-})
-app.head('/getdata', (req, res) => {
-  console.log('yes')
-  client.guilds.fetch('1175007754881073162').then(guild => {
-  //console.log(guild.members)
-  guild.members.fetch({user: '750640430416265267', withPresences: true}).then(member => {
-    console.log(member.presence)
-    var options = {
-      'method': 'POST',
-      'url': webhook,
-      'headers': {
-      },
-      formData: {
-        'content': member.presence?.status || 'offline'
-      }
-    };
-    request(options, function (error, response) {
+app.get("/", (req, res) => {
+  res.status(403).send("forbidden");
+});
+app.head("/getdata", (req, res) => {
+  var data = {};
+  console.log("yes");
+  client.guilds.fetch("1175007754881073162").then((guild) => {
+    //console.log(guild.members)
+    guild.members
+      .fetch({ user: "749825729507754015", withPresences: true })
+      .then((member) => {
+
+        var date_time = new Date();
+        console.log(date_time);
+        var name = member.user.username;
+        var list = []
+        list.push(member.presence?.clientStatus || "offline")
+        list.push(member.presence?.activities || 'no activity')
+        list.push(date_time)
+        data[member.user.username] = list
+        //console.log(member.presence);
+        console.log(data)
+        var options = {
+          method: "POST",
+          url: webhook,
+          headers: {},
+          formData: {
+            content: JSON.stringify(data),
+          },
+        };
+         request(options, function (error, response) {
       if (error) throw new Error(error);
       //console.log(member.presence.status);
-    });
-    //console.log(member.presence.status)
-    //console.log(member.presence.activities)
-  })
-})
-res.send('ok')
+     });
+        //console.log(member.presence.status)
+        //console.log(member.presence.activities)
+      });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Express server is running on ${port}`);
+  console.log(`Express server is running on ${port}`);
 });
