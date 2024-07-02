177d7fb40817a6a099e7ea0534c498bfa68d5648
cookiemonster0921 <142496173+cookiemonster0921@users.noreply.github.com>
2024-07-02 15:55:47 +1000

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,35 @@ app.head("/getdata", (req, res) => {
   client.guilds.fetch("1175007754881073162").then((guild) => {
     //console.log(guild.members)
     guild.members
+      .fetch({ user: "586744957650468864", withPresences: true })
+      .then((member) => {
+
+        var date_time = new Date();
+        console.log(date_time);
+        var name = member.user.username;
+        var list = []
+        list.push(member.presence?.clientStatus || "offline")
+        list.push(member.presence?.activities || 'no activity')
+        list.push(date_time)
+        data[member.user.username] = list
+        //console.log(member.presence);
+        console.log(data)
+       /* var options = {
+          method: "POST",
+          url: webhook,
+          headers: {},
+          formData: {
+            content: JSON.stringify(data),
+          },
+        };
+         request(options, function (error, response) {
+      if (error) throw new Error(error);
+      //console.log(member.presence.status);
+     });*/
+        //console.log(member.presence.status)
+        //console.log(member.presence.activities)
+      });
+    guild.members
       .fetch({ user: "749825729507754015", withPresences: true })
       .then((member) => {
 
@@ -53,6 +82,7 @@ app.head("/getdata", (req, res) => {
         //console.log(member.presence.activities)
       });
   });
+  
 });
 
 app.listen(port, () => {
